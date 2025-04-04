f9a79c16b2b8d37de6784bd666f3839f1caa0fe6
Tanmayee Chakraborty <tanmayee782@gmail.com>
2025-04-05 01:33:41 +0530

    New commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,19 @@
 import express from "express";
+import dotenv from 'dotenv'
+
+import {connectDB} from './lib/db.js'
 
 import authRoutes from './routes/auth.route.js'
+import { connect } from "mongoose";
 
+dotenv.config()
 const app = express();
+
+const PORT = process.env.PORT
 
 app.use("/api/auth", authRoutes)
 
-app.listen(5001, () => {
-    console.log("Server is running on port 5001");
+app.listen(PORT, () => {
+    console.log("Server is running on PORT:" + PORT);
+    connectDB()
 })
